bae384aa6bf1f2aeea2157367eee647835644b2a
Felix <fel98_98@mail.ru>
2021-08-11 21:06:31 +0200

    printed Perosn

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace my_git_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Ivan","Elbanov",23);
+            person.Print();
+        }
+    }
+}
