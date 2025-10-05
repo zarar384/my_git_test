e3850a3f386ea0f52c2af663b6c4813244511229
zarar384 <fel98_98@mail.ru>
2025-10-05 14:49:11 +0200

    simple demo for illustrate Discriminated Union type (Schrödinger Cat experiment)

---
+++ LeaveMeAloneFuncSkillForge.Test/SchrodingersCatTests.cs
@@ -0,0 +1,38 @@
+﻿using LeaveMeAloneFuncSkillForge.Domain.DiscriminatedUnions;
+using System;
+namespace LeaveMeAloneFuncSkillForge.Test
+{
+    public class SchrodingersCatTests
+    {
+        [Fact]
+        public void SchrodingersCat_AliveAndDeadStates_ShouldBeDistinct()
+        {
+            // Arrange
+            var aliveCat = new SchrodingersCat.Alive();
+            var deadCat = new SchrodingersCat.Dead();
+            
+            // Assert
+            Assert.IsType<SchrodingersCat.Alive>(aliveCat);
+            Assert.IsType<SchrodingersCat.Dead>(deadCat);
+            Assert.NotEqual<SchrodingersCat>(aliveCat, deadCat);
+        }
+
+        [Fact]
+        public void SchrodingersCat_PatternMatching_ShouldIdentifyStates()
+        {
+            // Arrange
+            SchrodingersCat cat = new SchrodingersCat.Alive();
+
+            // Act
+            string stateDescription = cat switch
+            {
+                SchrodingersCat.Alive => "The cat is alive.",
+                SchrodingersCat.Dead => "The cat is dead.",
+                _ => "Unknown state."
+            };
+
+            // Assert
+            Assert.Equal("The cat is alive.", stateDescription);
+        }
+    }
+}
---
+++ LeaveMeAloneFuncSkillForge/Domain/DiscriminatedUnions/SchrodingersCat.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaveMeAloneFuncSkillForge.Domain.DiscriminatedUnions
+{
+    /// <summary>
+    /// Simple demo for illustrate Discriminated Union type.
+    /// Represents Schrödinger Cat experiment with two possible states: Alive or Dead.
+    /// </summary>
+    public abstract record SchrodingersCat
+    {
+        public record Alive() : SchrodingersCat;
+        public record Dead() : SchrodingersCat;
+    }
+}
