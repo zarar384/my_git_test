c3f274cd928ca9c0fd460d4bf774c2e5ac36da5b
zarar384 <fel98_98@mail.ru>
2025-11-18 21:48:59 +0100

    Maybe Yes, Maybe No - DU Demo

---
+++ LeaveMeAloneFuncSkillForge.Test/MaybeDuTest.cs
@@ -0,0 +1,63 @@
+﻿using LeaveMeAloneFuncSkillForge.DiscriminatedUnions;
+using LeaveMeAloneFuncSkillForge.Domain;
+using LeaveMeAloneFuncSkillForge.Repositories.Interfaces;
+using Moq;
+
+namespace LeaveMeAloneFuncSkillForge.Test
+{
+    public class MaybeDuTest
+    {
+        [Fact]
+        public void NothingExample_ShouldReturnNoFilmFoundMessage()
+        {
+            // Arrange
+            var mockRepo = new Mock<IFilmRepository>();
+
+            // Act
+            var film = GetFilmFirstFilmByGenre("NonExistentGenre", mockRepo.Object);
+            var message = film switch
+            {
+                Something<Film> something => $"Found film: {something.Value.Title}",
+                Nothing<Film> _ => "No film found",
+                _ => throw new NotImplementedException(),
+            };
+
+            // Assert
+            Assert.Equal("No film found", message);
+        }
+
+        [Fact]
+        public void SomethingExample_ShouldReturnFoundFilmMessage()
+        {
+            // Arrange
+            var mockRepo = new Mock<IFilmRepository>();
+            mockRepo.Setup(r => r.GetFilmsByGenre("Drama")).Returns(new List<Film>
+            {
+                new Film { Title = "Film I", BoxOfficeRevenue = 200 },
+                new Film { Title = "Film HATE", BoxOfficeRevenue = 300 },
+                new Film { Title = "Film MYSELF", BoxOfficeRevenue = 100 }
+            });
+
+            // Act
+            var film = GetFilmFirstFilmByGenre("Drama", mockRepo.Object)!;
+            var message = film switch
+            {
+                Something<Film> something => $"Found film: {something.Value.Title}",
+                Nothing<Film> _ => "No film found",
+                _ => throw new NotImplementedException(),
+            };
+
+            // Assert
+            Assert.StartsWith("Found film: ", message);
+        }
+
+        private Maybe<Film> GetFilmFirstFilmByGenre(string genre, IFilmRepository filmRepository)
+        {
+            Maybe<Film> maybeFilm = filmRepository.GetFilmsByGenre(genre)
+                .FirstOrDefault() is { } film
+                    ? new Something<Film>(film)
+                    : new Nothing<Film>();
+            return maybeFilm;
+        }
+    }
+}
---
+++ LeaveMeAloneFuncSkillForge/DiscriminatedUnions/Maybe.cs
@@ -0,0 +1,21 @@
+﻿namespace LeaveMeAloneFuncSkillForge.DiscriminatedUnions
+{
+    public abstract class Maybe<T>
+    {
+    }
+
+    public sealed class  Something<T>: Maybe<T>
+    {
+        public Something(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; init; }
+    }
+
+    public sealed class Nothing<T> : Maybe<T>
+    {
+
+    }
+}
